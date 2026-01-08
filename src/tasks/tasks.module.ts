140e74dd33538681915703cad0675d953a13c0c5
stephano25 <nirina.r@zurcher.edu.mg>
2026-01-08 18:07:05 +0300

    correction de controller et module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 import { Task, TaskSchema } from './schema/task.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }])
+    MongooseModule.forFeature([
+      { name: Task.name, schema: TaskSchema }
+    ])
   ],
-  controllers: [TasksController],
-  providers: [TasksService],
+  controllers: [TasksController], // ⬅️ OBLIGATOIRE
+  providers: [TasksService],       // ⬅️ OBLIGATOIRE
 })
 export class TasksModule {}
